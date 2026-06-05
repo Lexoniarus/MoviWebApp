cafdedc06de25204e6e6933bf42bef3546b9845f
Lexoniarus <alexander.altmann90@gmail.com>
2026-06-05 11:52:35 +0200

    Set up Flask app and database

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask
 
+from data_manager import DataManager
+from models import Movie, db
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///moviwebapp.db"
+
+db.init_app(app)
+data_manager = DataManager()
+
+
+@app.route("/")
+def home():
+    return "Welcome to MoviWeb App!"
+
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run()
